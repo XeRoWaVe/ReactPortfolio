940928c2a38669ad890c29ddaf1040da14ea1289
Aaron Craig <xerowave22@hotmail.com>
2024-04-14 11:36:36 -0500

    Updated buttons

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <li className="px-4 lg:absolute top-4 right-[40%]">
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500 text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
+            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500 text-white bg-[#0D0D0D] border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
           >
             Home
           </NavLink>
@@ -14,7 +14,7 @@ const Nav = () => {
         <li className="px-4 lg:absolute top-4 right-[30%]">
           <NavLink
             to="/skills"
-            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500 text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
+            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500 text-white bg-[#0D0D0D] border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
           >
             Skills
           </NavLink>
@@ -22,7 +22,7 @@ const Nav = () => {
         <li className="px-4 lg:absolute top-4 right-[20%]">
           <NavLink
             to="/projects"
-            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500 text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
+            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500 text-white bg-[#0D0D0D] border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
           >
             Projects
           </NavLink>
@@ -30,7 +30,7 @@ const Nav = () => {
         <li className="px-4 lg:absolute top-4 right-[10%]">
           <NavLink
             to="/contact"
-            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500  text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
+            className={({ isActive }) => (isActive ? "inline-flex text-white bg-green-500   border-0 py-2 px-6 focus:outline-none  rounded text-lg" : "ml-4 inline-flex  hover:bg-gray-500  text-white bg-[#0D0D0D] border-0 py-2 px-6 focus:outline-none dark:hover:bg-gray-700 dark:hover:text-white rounded text-lg")}
           >
             Contact
           </NavLink>
