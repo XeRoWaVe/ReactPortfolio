8bea6243c514be5c599499bb96f5d277199b3a7c
Aaron Craig <xerowave22@hotmail.com>
2024-04-15 11:34:30 -0500

    Updated hire me functionality

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -91,18 +91,6 @@ const Contact = () => {
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="caret-violet-500 w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="relative mb-4">
             <label
               htmlFor="message"
               className="leading-7 text-sm text-gray-400"
@@ -116,12 +104,14 @@ const Contact = () => {
               onChange={(e) => setMessage(e.target.value)}
             />
           </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          <a
+            href={`mailto:xerowave22@hotmail.com?subject=${name}&body=${message}`}
+            className="text-white bg-indigo-500 border-0  py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg w-fit"
+            title="Opens default mail client"
           >
             Submit
-          </button>
+          </a>
+          
         </form>
       </div>
     </section>
