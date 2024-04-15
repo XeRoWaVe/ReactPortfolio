4ac408618acbfb0b0dca93929ab47a2906ad78ac
Aaron Craig <xerowave22@hotmail.com>
2024-04-15 12:30:39 -0500

    Updated text on contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -73,10 +73,10 @@ const Contact = () => {
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Shoot me an email
           </h2>
           <p className="leading-relaxed mb-5 text-gray-100">
-            I am currently looking for a Junior Web Developer position to begin my professional journey.
+            I am currently looking for opportunities to grow as a developer. Send me your ideas and we'll talk.
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
