8a32977969e9745d313eec6cad0efc940eb5225b
Aaron Craig <xerowave22@hotmail.com>
2024-04-15 13:22:24 -0500

    removed personal info from site

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -43,26 +43,18 @@ const Contact = () => {
             src="https://www.google.com/maps/embed/v1/place?q=1110+Burgess+Ct,+Arlington,+TX,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900  relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1 text-white">
-                1110 Burgess Ct. <br />
-                Arlington, TX 76015
-              </p>
-            </div>
+
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href="mailto:xerowave22@hotmail.com" className="text-indigo-400 leading-relaxed">
                 xerowave22@hotmail.com
               </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+              {/* <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed text-white">817-821-5544</p>
+              <p className="leading-relaxed text-white">817-821-5544</p> */}
             </div>
           </div>
         </div>
